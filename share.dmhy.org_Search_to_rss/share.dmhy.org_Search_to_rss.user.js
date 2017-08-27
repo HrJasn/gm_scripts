c6c6569ac61ff69fd744d1caf4e163a946ff8b5f
HrJasn <hrjasn@gmail.com>
2017-08-27 19:19:09 +0800

    Initial commit

---
+++ share.dmhy.org_Search_to_rss/share.dmhy.org_Search_to_rss.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        share.dmhy.org Search to rss
+// @namespace   rss.dmhy.org.Search
+// @description 將動漫花園資源網的搜尋轉為rss訂閱網址
+// @include     http*://share.dmhy.org/topics/list?keyword=*
+// @version     1.1
+// @grant       none
+// ==/UserScript==
